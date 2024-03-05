3cda466d7aeac02c50bbae01c6e8166ac931e624
thiago <thiagotsdelima@gmail.com>
2024-03-04 22:47:09 -0300

    update

---
+++ src/screens/Home/index.tsx
@@ -0,0 +1,17 @@
+import { Text, View, TextInput } from 'react-native';
+
+import { styles } from './styles';
+
+export function Home() {
+  return (
+    <View style={styles.container}> 
+      <Text style={styles.eventName}>Nome do evento</Text> 
+      <Text style={styles.eventData}>sexta, 4 de Novembro de 2022</Text>
+      <TextInput 
+      style={styles.input}
+      placeholder="Nome do participante"
+      placeholderTextColor="#FFF"
+      />
+    </View>
+  );
+}
