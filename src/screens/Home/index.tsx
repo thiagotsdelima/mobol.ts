ecec1448597c382fe75ba3c8289b4fd593a463df
thiago <thiagotsdelima@gmail.com>
2024-03-05 22:06:05 -0300

    update

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,17 +1,25 @@
-import { Text, View, TextInput } from 'react-native';
+import { Text, View, TextInput, TouchableOpacity } from 'react-native';
 
 import { styles } from './styles';
 
 export function Home() {
+  
+  function handleParticipantAdd() {
+    alert("Você clicou no botão");
+  }
+
   return (
     <View style={styles.container}> 
       <Text style={styles.eventName}>Nome do evento</Text> 
-      <Text style={styles.eventData}>sexta, 4 de Novembro de 2022</Text>
+      <Text style={styles.eventData}>Sexta, 4 de Novembro de 2022</Text>
       <TextInput 
-      style={styles.input}
-      placeholder="Nome do participante"
-      placeholderTextColor="#FFF"
+        style={styles.input}
+        placeholder="Nome do participante"
+        placeholderTextColor="#FFF"
       />
+     <TouchableOpacity style={styles.button} onPress={handleParticipantAdd}> 
+        <Text style={styles.buttonText}>+</Text>
+      </TouchableOpacity>
     </View>
   );
 }
